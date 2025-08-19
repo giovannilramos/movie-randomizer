8eeaafb875d5181fad9ddfe7fe9ed7d7aad62c33
Giovanni Ramos <giovannilramos55@gmail.com>
2025-08-18 21:30:08 -0300

    refactor: class refactor

--- src/main/java/br/com/giovanniramos/movierandomizer/services/usecases/LastWatchedMovieUseCase.java
+++ src/main/java/br/com/giovanniramos/movierandomizer/services/usecases/LastWatchedMovieUseCase.java
@@ -3,31 +3,20 @@ package br.com.giovanniramos.movierandomizer.services.usecases;
 import br.com.giovanniramos.movierandomizer.exceptions.NotFoundException;
 import br.com.giovanniramos.movierandomizer.models.MovieModel;
 import br.com.giovanniramos.movierandomizer.repositories.LastMovieDrawRepository;
-import br.com.giovanniramos.movierandomizer.repositories.MovieRepository;
-import br.com.giovanniramos.movierandomizer.services.MinioService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-
-import static br.com.giovanniramos.movierandomizer.mappers.MovieMapper.MOVIE_MAPPER;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class LastWatchedMovieUseCase {
     private final LastMovieDrawRepository lastMovieDrawRepository;
-    private final MovieRepository movieRepository;
-    private final MinioService minioService;
+    private final GetMovieDetailsUseCase getMovieDetailsUseCase;
 
     public MovieModel execute() {
         return lastMovieDrawRepository.findFirstBy()
-                .map(lastMovieDrawEntity -> MOVIE_MAPPER.mapToMovieModelFromMovieEntity(
-                        movieRepository.findById(lastMovieDrawEntity.getLastDrawMovieId()).orElse(null))
-                ).map(this::getMovieCover)
+                .map(lastMovieDrawEntity -> getMovieDetailsUseCase.execute(lastMovieDrawEntity.getLastDrawMovieId()))
                 .orElseThrow(() -> new NotFoundException("No movies watched yet"));
-    }
-
-    private MovieModel getMovieCover(final MovieModel movieModel) {
-        return movieModel.toBuilder().movieCoverUrl(minioService.getMinioObjectUrl(movieModel.getMovieCoverId())).build();
     }
 }
